c2ea63d5bbb68f84358c327cff04bda9da7379d2
mrwho95 <dnkh903@gmail.com>
2020-07-13 05:51:13 +0800

    send message to server and able to receive message from server

--- public/chat.js
+++ public/chat.js
@@ -1,2 +1,25 @@
 //Make connection
 var socket = io.connect('http://localhost:4000');
+
+//Query DOM
+//get input message on front end
+var message = document.getElementById('message');
+var handle = document.getElementById('handle');
+var btn = document.getElementById('send');
+var output = document.getElementById('output');
+
+//emit events
+btn.addEventListener('click', function(){
+	//first parameter is name of the message
+	//second parameter is data of the message need to send to server
+	socket.emit('chat', {
+		//object
+		message: message.value,
+		handle: handle.value
+	});
+});
+
+//listen for send events from server
+socket.on('chat', function(data){
+	output.innerHTML += '<p><strong>' + data.handle + ': </strong>' + data.message + '</p>';
+})
