11952253c1bb28ff244b1e040569887a7ef015cc
mrwho95 <dnkh903@gmail.com>
2020-07-13 05:14:24 +0800

    build socket by socket.io library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var socket = require('socket.io');
 
 //App setup
 var app = express();
@@ -9,3 +10,12 @@ var server = app.listen(4000, function(){
 //static files
 //go to index.html auto 
 app.use(express.static('public'));
+
+//socket setup
+//socket io works on this server
+var io = socket(server);
+
+io.on('connection', function(socket){
+	//both server and client front end established the socket then it will appear result below
+	console.log('made socket connection', socket.id);
+});
